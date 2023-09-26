568edacc0797d2f8975d6a1cc9cc6c3d36f3a7fa
David Kovlak <kovlakdavid4@gmail.com>
2023-09-26 13:21:25 -0700

    Done

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace APIsAndJSON
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            int n = 10;
+            do
+            {
+                n -= 1;
+                QuoteGenerator.KanyeQuote();
+                Console.WriteLine();
+
+                QuoteGenerator.RonQuote();
+                Console.WriteLine();
+
+            } while (n > 0);
         }
+ 
     }
 }
---
+++ APIsAndJSON/QuoteGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
+{
+	public class QuoteGenerator
+	{
+		public static void KanyeQuote()
+		{
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse);
+
+            Console.WriteLine($"Kanye West: {kanyeQuote["quote"]}");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);
+            
+            Console.WriteLine($"Ron Swanson: {ronQuote[0]}");
+        }
+
+    }
+}
+
